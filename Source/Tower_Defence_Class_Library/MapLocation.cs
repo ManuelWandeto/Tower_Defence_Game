7a492b00b12a1c3db5481df520f20534f5378747
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-26 00:22:55 +0300

    Add a MapLocation class which is a subclass of the Point class

---
+++ Source/Tower_Defence_Class_Library/MapLocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public class MapLocation : Point
+    {
+        public MapLocation(int x, int y, Map map) : base(x,y)
+        {
+            if(!map.OnMap(this))
+            {
+                throw new Exception("This point is not on the map");
+            }
+        }
+    }
+}
