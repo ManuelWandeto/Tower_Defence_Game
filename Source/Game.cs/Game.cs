abdc46f0fe6829c4fa13319a30ed8e679d355d89
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-25 21:46:32 +0300

    Add a Tower class to the .net standard library and a start up game project  with the main program class game.cs

---
+++ Source/Game.cs/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Tower_Defence_Class_Library;
+namespace Game
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
