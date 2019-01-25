4579af596e92136f906da6e04d17b338199912d5
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-26 01:12:36 +0300

    Add the Exceptions class containing custom exception classes for the tower defence game

---
+++ Source/Tower_Defence_Class_Library/Exceptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public class Tower_Defence_Exception : Exception
+    {
+        public Tower_Defence_Exception() { }
+        public Tower_Defence_Exception(string ErrorMessage): base(ErrorMessage) { }
+    }
+
+    public class Out_Of_Bounds_Exception : Tower_Defence_Exception
+    {
+        public Out_Of_Bounds_Exception() { } //automatically calls the base() default constructor
+        public Out_Of_Bounds_Exception(string ErrorMessage) : base(ErrorMessage) { }
+    }
+}
