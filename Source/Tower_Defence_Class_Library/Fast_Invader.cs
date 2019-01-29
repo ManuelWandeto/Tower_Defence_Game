f4d2b3288d2303ed96a6b2514b80d0c6e14314a8
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-29 23:14:04 +0300

    Add the Fast_Invader class

---
+++ Source/Tower_Defence_Class_Library/Fast_Invader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public class Fast_Invader : Invader
+    {
+        public Fast_Invader(Path path) : base(path) { }
+
+        protected override int StepSize { get; } = 2;
+    }
+}
