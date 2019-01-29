1b8e3cb5080e02dc48b4bb7b4eeb55da87876d79
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-30 00:25:02 +0300

    Update the Point class

--- Source/Tower_Defence_Class_Library/Point.cs
+++ Source/Tower_Defence_Class_Library/Point.cs
@@ -19,7 +19,27 @@ namespace Tower_Defence_Class_Library
             return (int)Math.Sqrt(Math.Pow((x - X), 2) + Math.Pow((y - Y), 2)); 
 
         }
-
+        public override bool Equals(object obj)
+        {
+            if(obj is Point)
+            {
+                Point that = (Point)obj;
+                if(X == that.X && Y == that.Y)
+                {
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() * 31 + Y.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}{1},{2}{3}", "(", X, Y , ")");
+        }
         public int DistanceTo(Point point)
         {
             return DistanceTo(point.X, point.Y);
