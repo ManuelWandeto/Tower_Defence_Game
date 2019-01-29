699dab80d4b701365a911a36ba2bcac93e82218a
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-29 15:36:10 +0300

    Update the Invader class

--- Source/Tower_Defence_Class_Library/Invader.cs
+++ Source/Tower_Defence_Class_Library/Invader.cs
@@ -6,6 +6,20 @@ namespace Tower_Defence_Class_Library
 {
     public class Invader
     {
-        public MapLocation Location { get; private set; }
+        private int pathStep = 0; //fields are initialised first before the constructor is called
+        private readonly Path _path;
+        public int health { get; private set; } = 2;
+        public MapLocation Location => _path.GetLocationAt(pathStep);
+
+        public Invader(Path path)
+        {
+            _path = path;
+        }
+
+        public void Move() => pathStep++;
+        public void DecreaseHealth(int factor) => health -= factor;
+        public bool IsNeutralised => health <= 0; //a computed property that returns a value based on the logic defined
+        public bool HasScored => pathStep >= (_path.Length);
+        public bool IsActive => !(HasScored && IsNeutralised);
     }
 }
