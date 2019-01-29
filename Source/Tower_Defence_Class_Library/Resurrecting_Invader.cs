4d8406ba38a3f46111b0bdcbc3c3df8ae343309f
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-29 23:16:08 +0300

    Add the Resurrecting_Invader class awaiting implementation

---
+++ Source/Tower_Defence_Class_Library/Resurrecting_Invader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public class Resurrecting_Invader : Invader
+    {
+        public Resurrecting_Invader(Path path) : base(path) {}
+
+    }
+}
