c563c64e2ee23390f2a5ebb1d67970eecd4ee7ed
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-29 23:17:21 +0300

    Add the Strong_Invader class

---
+++ Source/Tower_Defence_Class_Library/Strong_Invader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public class Strong_Invader: Invader
+    {
+        public Strong_Invader(Path path) : base(path) { }
+        public override int Health { get; protected set; } = 2;
+    }
+}
