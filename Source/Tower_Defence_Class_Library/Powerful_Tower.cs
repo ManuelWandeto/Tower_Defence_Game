af88095f092028939bf0c3a9bc60b0f3bd62fe15
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-29 23:15:35 +0300

    Add the Powerful_Tower class

---
+++ Source/Tower_Defence_Class_Library/Powerful_Tower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public class Powerful_Tower : Tower
+    {
+        public Powerful_Tower(MapLocation location, Path path) : base(location, path) { }
+        protected override int power { get; } = 2;
+    }
+}
