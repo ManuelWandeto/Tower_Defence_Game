3de1b156b7ef73e0bfe66b017fc992400c049d1c
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-29 23:13:42 +0300

    Add the Basic_Invader class

---
+++ Source/Tower_Defence_Class_Library/Basic_Invader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public class Basic_Invader : Invader
+    {
+        public Basic_Invader(Path path) : base(path) { }
+    }
+}
