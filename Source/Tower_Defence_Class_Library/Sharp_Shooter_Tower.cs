cbc1ed9a31903613d71cca0a966240d9f7f15dc1
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-29 23:16:43 +0300

    Add the Sharp_Shooter class that is a type of tower

---
+++ Source/Tower_Defence_Class_Library/Sharp_Shooter_Tower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public class Sharp_Shooter_Tower : Tower
+    {
+        public Sharp_Shooter_Tower(MapLocation location, Path path) : base(location, path) { }
+
+        protected override double Accuracy { get; } = .90;
+    }
+}
