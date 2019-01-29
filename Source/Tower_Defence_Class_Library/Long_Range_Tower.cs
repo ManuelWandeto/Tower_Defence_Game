ec86b7d098b89f3bc8b6c4aa946b9c3ba795b00f
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-29 23:14:59 +0300

    Add the Long_Range_Tower class

---
+++ Source/Tower_Defence_Class_Library/Long_Range_Tower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public class Long_Range_Tower : Tower
+    {
+        public Long_Range_Tower(MapLocation location, Path path) : base(location, path) { }
+
+        protected override int range { get; } = 3;
+    }
+}
