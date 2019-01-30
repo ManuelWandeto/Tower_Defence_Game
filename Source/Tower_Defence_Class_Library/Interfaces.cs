8a86906a0edadaccc850a3f35181fa3ca6c38b77
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-30 03:41:14 +0300

    Add the Interfaces class to hold all type interfaces

---
+++ Source/Tower_Defence_Class_Library/Interfaces.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public interface ITower
+    {
+        void FireOnInvaders(IInvader[] invaders);
+    }
+
+    public interface IMappable
+    {
+        MapLocation Location { get; }
+    }
+    
+    public interface IMovable
+    {
+        void Move();
+    }
+    public interface IInvader : IMappable, IMovable //intefaces define what public properties and methods are and what their return types are
+    {
+        int Health { get; }
+        void DecreaseHealth(int factor);
+        bool IsNeutralised { get; } //a computed property that returns a value based on the logic defined
+        bool HasScored { get; }
+        bool IsActive { get; }
+    }
+}
