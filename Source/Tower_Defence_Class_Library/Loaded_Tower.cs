ca8c99e91470b3f7c0387e914a1afb0617fdc1ce
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-30 03:42:17 +0300

    Add the Loaded_Tower class that is another type of tower

---
+++ Source/Tower_Defence_Class_Library/Loaded_Tower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public class Loaded_Tower : Tower
+    {
+        private int ammunition = 3;
+        public Loaded_Tower(MapLocation location, Path path) : base(location, path) { }
+
+        public override void FireOnInvaders(IInvader[] invaders)
+        {
+            int remainingAmmunition = ammunition;
+            for(int i = 1; i <= ammunition; i++)
+            {
+                base.FireOnInvaders(invaders);
+                remainingAmmunition -= 1;
+                Console.WriteLine("Loaded tower shot {0} rounds, remaining rounds {1}", i , remainingAmmunition);
+            }
+            ammunition = 0;
+        }
+
+
+    }
+}
