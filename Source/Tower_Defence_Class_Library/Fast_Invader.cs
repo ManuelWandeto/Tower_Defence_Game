c5262ab7f1ed9139017f6d44d50e196ee694d37e
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-30 03:40:44 +0300

    Update the Fast_Invader class

--- Source/Tower_Defence_Class_Library/Fast_Invader.cs
+++ Source/Tower_Defence_Class_Library/Fast_Invader.cs
@@ -7,6 +7,7 @@ namespace Tower_Defence_Class_Library
     public class Fast_Invader : Invader
     {
         public Fast_Invader(Path path) : base(path) { }
+        public override int Health { get; protected set; } = 2;
 
         protected override int StepSize { get; } = 2;
     }
