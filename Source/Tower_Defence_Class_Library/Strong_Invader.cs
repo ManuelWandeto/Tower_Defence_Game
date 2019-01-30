3c68dce3c703ebbd181fd831ffb2a5857e8876e6
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-30 03:44:15 +0300

    Update the Strong_Invader class

--- Source/Tower_Defence_Class_Library/Strong_Invader.cs
+++ Source/Tower_Defence_Class_Library/Strong_Invader.cs
@@ -7,6 +7,6 @@ namespace Tower_Defence_Class_Library
     public class Strong_Invader: Invader
     {
         public Strong_Invader(Path path) : base(path) { }
-        public override int Health { get; protected set; } = 2;
+        public override int Health { get; protected set; } = 3;
     }
 }
