161955a6742592cb3bfff4e4d096a6a70c3374dd
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-30 03:40:26 +0300

    Update the Basic_Invader class

--- Source/Tower_Defence_Class_Library/Basic_Invader.cs
+++ Source/Tower_Defence_Class_Library/Basic_Invader.cs
@@ -7,5 +7,6 @@ namespace Tower_Defence_Class_Library
     public class Basic_Invader : Invader
     {
         public Basic_Invader(Path path) : base(path) { }
+        public override int Health { get; protected set; } = 2;
     }
 }
