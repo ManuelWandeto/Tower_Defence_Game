0829a978dba40dbec0b5f1d6c18d917c661a8005
Manuel Wandeto <immanuelmojo@hotmail.com>
2019-01-30 03:43:07 +0300

    Add a static Random class to hold one global Random object

---
+++ Source/Tower_Defence_Class_Library/Random.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defence_Class_Library
+{
+    public static class Random
+    {
+        private static System.Random _Random = new System.Random();
+
+        public static double NextDouble() => _Random.NextDouble();
+    }
+}
